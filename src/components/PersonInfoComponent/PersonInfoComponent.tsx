fd19af225e7b63e78c5aae8ece1e7fe3da55f495
Rahul <mail.rahulbhoir@gmail.com>
2025-07-06 12:32:26 +0530

    added image place holder

--- src/components/PersonInfoComponent/PersonInfoComponent.tsx
+++ src/components/PersonInfoComponent/PersonInfoComponent.tsx
@@ -1,29 +1,49 @@
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import type { IPersonInfo } from "@/constants/interface";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { AvatarFallback } from "@radix-ui/react-avatar";
-import { Avatar } from "../ui/avatar";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { ArrowLeft } from "lucide-react";
+import { Button } from "../ui/button";
 
 const PersonInfoComponent = () => {
+  const navigate = useNavigate();
+
   const location = useLocation();
   const person: IPersonInfo = location.state;
   console.log("personInfo", person);
 
   return (
-    <Card className="m-4 w-full max-w-sm mx-auto rounded-2xl shadow-md border">
-      <CardHeader className="flex flex-col items-center">
-        <Avatar className="h-20 w-20 mb-2">
-          <AvatarFallback>
-            {person.name
-              .split(" ")
-              .map((w) => w[0])
-              .join("")}
-          </AvatarFallback>
-        </Avatar>
-        <CardTitle className="text-center text-xl font-bold">{person.name}</CardTitle>
-        <p className="text-sm text-muted-foreground">
-          {person.gender} — {person.birth_year}
-        </p>
+    <Card className="w-full m-4 max-w-sm mx-auto rounded-2xl shadow-md border">
+      <CardHeader className="space-y-4">
+        <div>
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => navigate(-1)}
+            className="flex items-center gap-2 text-muted-foreground hover:text-primary underline cursor-pointer"
+          >
+            <ArrowLeft className="h-4 w-4" />
+            Back
+          </Button>
+        </div>
+
+        {/* Avatar & Title */}
+        <div className="flex flex-col items-center">
+          <Avatar className="h-20 w-20 mb-2">
+            <AvatarImage src={`https://ui-avatars.com/api/?name=${encodeURIComponent(person.name)}&background=random`} alt={person.name} />
+            <AvatarFallback>
+              {person.name
+                .split(" ")
+                .map((w) => w[0])
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
+          <CardTitle className="text-center text-xl font-bold">{person.name}</CardTitle>
+          <p className="text-sm text-muted-foreground">
+            {person.gender} — {person.birth_year}
+          </p>
+        </div>
       </CardHeader>
 
       <CardContent className="text-sm text-gray-700 space-y-3">
@@ -32,16 +52,10 @@ const PersonInfoComponent = () => {
             <span className="font-medium">Height:</span> {person.height} cm
           </p>
           <p>
-            <span className="font-medium">Mass:</span> {person.mass} kg
-          </p>
-          <p>
             <span className="font-medium">Hair:</span> {person.hair_color}
           </p>
           <p>
             <span className="font-medium">Eyes:</span> {person.eye_color}
-          </p>
-          <p>
-            <span className="font-medium">Skin:</span> {person.skin_color}
           </p>
           <p>
             <span className="font-medium">Homeworld:</span> {person.planet.name}
